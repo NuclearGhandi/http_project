79aa29bb2f412a3e6577aafa151350ace5becc69
NuclearGhandi <igorr.kovalev@gmail.com>
2023-07-21 21:28:38 +0300

    iter7

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"math/rand"
@@ -20,6 +21,13 @@ type Config struct {
 type Runtime struct {
 	keytoURLMap map[string]string
 	sugar       zap.SugaredLogger
+}
+
+type inputJSON struct {
+	URL string `json:"url"`
+}
+type outputJSON struct {
+	URL string `json:"result"`
 }
 
 var cfg Config
@@ -82,6 +90,29 @@ func handlePOST(c *gin.Context) {
 		}
 	}
 }
+func handleApiPOST(c *gin.Context) {
+	fmt.Println("API")
+	var inpt inputJSON
+	var outpt outputJSON
+	body, err := c.GetRawData()
+	if err != nil {
+		fmt.Println("API_body_get_err")
+		serverErr(c)
+	}
+	if err = json.Unmarshal(body, &inpt); err != nil {
+		fmt.Println("API_Unmsrshall_err")
+		fmt.Println(err)
+		serverErr(c)
+	}
+	outpt.URL = addURL(inpt.URL)
+	resp, err := json.Marshal(outpt)
+	if err != nil {
+		fmt.Println("API_Marshall_err")
+		serverErr(c)
+	} else {
+		c.Data(http.StatusCreated, "application/json", resp)
+	}
+}
 
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -97,10 +128,10 @@ func Logger() gin.HandlerFunc {
 		size := c.Writer.Size()
 		rnt.sugar.Infoln(
 			"uri", uri,
-			"method", method,
-			"status", status,
-			"duration", duration,
-			"size", size,
+			"| method", method,
+			"| status", status,
+			"| duration", duration,
+			"| size", size,
 		)
 	}
 }
@@ -108,10 +139,14 @@ func setupRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(Logger())
 	r.Use(gin.Recovery())
+
 	r.GET("/:key", handleGET)
 	r.POST("/", handlePOST)
+
 	r.POST("/:key", serverErr)
 	r.GET("/", serverErr)
+	r.POST("/api/shorten", handleApiPOST)
+	r.GET("/api/:key", serverErr)
 	return r
 }
 
